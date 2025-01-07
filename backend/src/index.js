93e6e1d543074fd5f53c1a571c43842c109ffb46
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-07 16:48:12 +0530

    initial controller and routes created and the mongodb connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
 
+import authRouter from './routes/auth.route.js';
+
+dotenv.config();
 const app = express();
 
-app.listen(5001,() => {
-    console.log("Server is running on post 5001");
+const PORT = process.env.PORT;
+
+app.use('/api/auth', authRouter);
+
+app.listen(PORT,() => {
+    console.log(`Server is running on post ${PORT}`);
+    connectDB();
 })
