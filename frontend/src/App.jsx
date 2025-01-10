de69003ca6166112fd240169273d4af217be628e
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-11 01:37:54 +0530

    The checkAuth method is created in store and called in Navbar and testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,19 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 const Hello = () => {
+
+  const {  authUser, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth]);
+
+  console.log("user : ",authUser);
+
   return (
     <div>
       <Navbar />
