c87f19eb0521b755dab56066a62f1dce2cedb689
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-10 23:47:29 +0530

    auth routes created and checked

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import { generateToken } from "../lib/utils.js"
+import { generateToken } from "../lib/utils.js";
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req,res) => {
     const { fullName , email , password } = req.body;
@@ -103,5 +104,33 @@ export const logout = (req,res) => {
     }catch(error){
         console.log("error in logout controller.");
         return req.status(500).json({ message : "Internal server error." })
+    }
+}
+
+export const updateProfile = async (req,res) => {
+    try{
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({ message : "Profile image is required." });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updateUser = await User.findByIdAndUpdate(userId, {profilePic : uploadResponse.secure_url}, {new : true});
+
+        return res.status(200).json(updateUser);
+    }catch(error){
+        console.log("error in user profile image updation controller", error);
+        return res.status(500).json({ message : "Internal server error." });
+    }
+}
+
+export const checkAuth = async (req,res) => {
+    try{
+        res.status(200).json(req.user);
+    }catch(error){
+        console.log("error in checkAuth controller.",error);
+        return res.status(500).json({ message : "Internal server error." });
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
 
 import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
