4726f4d17fc4a6ba35c03582f0d6f5407193a8e3
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-11 01:42:27 +0530

    in index.sj cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
 
 const PORT = process.env.PORT;
 
