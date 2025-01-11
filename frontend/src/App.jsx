569500906a716bd532f02399acb8bccd8bf5e388
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-11 16:55:08 +0530

    sign up funcationality implemented with signuppage designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
 
 const Hello = () => {
 
@@ -16,8 +17,6 @@ const Hello = () => {
   useEffect(() => {
     checkAuth();
   },[checkAuth]);
-
-  console.log("user : ",authUser);
 
   if(isCheckingAuth && !authUser) return(
     <div className="flex items-center justify-center h-screen">
@@ -35,6 +34,8 @@ const Hello = () => {
         <Route path="/settings" element={ <SettingsPage />}/>
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" /> } />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
