03fc47cb817fcf60486a63573d1181bbf5695c85
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-13 02:05:05 +0530

    lgin function in store implemented and the login page design added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -34,6 +34,19 @@ export const useAuthStore = create((set) => ({
         }
     },
 
+    login : async (data) => {
+        set({ isLogginIng : true });
+        try{
+            const res = await axiosInstance.post("/auth/login",data);
+            set({ authUser : res.data });
+            toast.success("Logged in successfullt.");
+        }catch(error){
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLogginIng : false });
+        }
+    },
+
     logout : async () => {
         try{
             await axiosInstance.post("/auth/logout");
