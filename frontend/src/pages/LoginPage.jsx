0a736c5f99606b9f0bb4e2f0818bb9dc9acc720f
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-12 20:21:36 +0530

    login page updating

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react"
+import { useAuthStore } from "../store/useAuthStore";
+
 const LoginPage = () => {
+  const [ showPassword, setShowPassword ] = useState(false);
+  const [ formData, setFormData ] = useState({
+    email :"",
+    password : ""
+  });
+
+  const { login, isLogingIn } = useAuthStore();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    login(formData);
+  };
+  
   return (
     <div>LoginPage</div>
   )
