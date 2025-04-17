6fde985a2b45ef7cb2db2ca5f210194b8809bebe
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-04-17 21:57:57 +0530

    Notification scrolling bug fixed

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -5,9 +5,9 @@ import Suggestions from "../components/sidebars/Suggestions";
 import { useNotificationStore } from "../store/useNotificationStores";
 
 const NotificationsPage = () => {
-  
   const { getSearchSelectedUser } = useSearchStore();
-  const { notifications, notificationsLoading, getNotifications } = useNotificationStore();
+  const { notifications, notificationsLoading, getNotifications } =
+    useNotificationStore();
 
   const handleViewUser = (id, e) => {
     e.preventDefault();
@@ -20,30 +20,30 @@ const NotificationsPage = () => {
   }, []);
 
   return (
-    <div className="w-10/12 flex">
-        <div className="w-8/12">
-          {notificationsLoading ? (
-            <div className="flex justify-center items-center w-full px-4 py-8 h-screen">
-              <span className="loading loading-bars loading-lg"></span>
-            </div>
-          ) : notifications && notifications.length > 0 ? (
-            <div className="flex flex-col justify-center items-center w-full px-4 py-8">
-              {notifications.map((notification) => (
-                <NotificationBar
-                  key={notification._id}
-                  user={notification.fromUserId}
-                  message={notification.message}
-                  onClick={(id, e) => handleViewUser(id, e)}
-                  time={notification.createdAt}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="flex justify-center w-full py-4 h-full items-center">
-              <p>No notifications yet.</p>
-            </div>
-          )}
-        </div>
+    <div className="w-10/12 flex h-full">
+      <div className="w-8/12 overflow-y-scroll no-scrollbar">
+        {notificationsLoading ? (
+          <div className="flex justify-center items-center w-full px-4 py-8 h-screen">
+            <span className="loading loading-bars loading-lg"></span>
+          </div>
+        ) : notifications && notifications.length > 0 ? (
+          <div className="flex flex-col justify-center items-center w-full px-4 py-8 space-y-2">
+            {notifications.map((notification) => (
+              <NotificationBar
+                key={notification._id}
+                user={notification.fromUserId}
+                message={notification.message}
+                onClick={(id, e) => handleViewUser(id, e)}
+                time={notification.createdAt}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center w-full py-4 h-full items-center">
+            <p>No notifications yet.</p>
+          </div>
+        )}
+      </div>
       <Suggestions />
     </div>
   );
