bc0ada46d390079a44db9e55d5f69ba07e488900
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-04-18 16:05:39 +0530

    Mobile view bottom and top bar designed

---
+++ frontend/src/components/sidebars/MobileBottomBar.jsx
@@ -0,0 +1,48 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/useAuthStore";
+import { useSearchStore } from "../../store/useSearchStore";
+import {
+  Home,
+  LogOut,
+  PlusSquare,
+  Search,
+} from "lucide-react";
+
+const MobileBottomBar = () => {
+  const navigate = useNavigate();
+  const { logout, authUser } = useAuthStore();
+  const { getSearchSelectedUser } = useSearchStore();
+
+  return (
+    <footer className="bg-base-100 border-t border-base-300 w-full z-40 backdrop-blur-lg md:hidden">
+      {authUser && (
+        <div className="flex justify-around">
+          <button className="btn bg-base-100 border-0" onClick={logout}>
+            <LogOut className="size-5" />
+          </button>
+
+          <Link to={"/"} className={`btn bg-base-100 border-0`}>
+            <Home className="size-5" />
+          </Link>
+
+          <Link to={"/createPost"} className={`btn bg-base-100 border-0`}>
+            <PlusSquare className="size-5" />
+          </Link>
+
+          <Link to={"/search"} className={`btn bg-base-100 border-0`}>
+            <Search className="size-5" />
+          </Link>
+
+          <button
+            className={`btn bg-base-100 border-0`}
+            onClick={() => getSearchSelectedUser(authUser._id, navigate)}
+          >
+            <img src={authUser.profilePic || '/user_avatar.jpg'} className="size-6 rounded-full"/>
+          </button>
+        </div>
+      )}
+    </footer>
+  );
+};
+
+export default MobileBottomBar;
