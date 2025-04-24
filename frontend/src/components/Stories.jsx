204f75d5174767db783d7ca0206a99fc99b65053
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-04-24 17:39:06 +0530

    My story div h and w updated

--- frontend/src/components/Stories.jsx
+++ frontend/src/components/Stories.jsx
@@ -33,7 +33,7 @@ const Stories = () => {
           <div className="p-[4px] bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 rounded-full inline-block">
             {myStory ? (
               <div
-              className="relative group"
+              className="relative group h-14 w-14 md:h-16 md:w-16 lg:h-20 lg:w-20"
               onMouseEnter={() => setStoryHover(true)}
               onMouseLeave={() => setStoryHover(false)}
             >
